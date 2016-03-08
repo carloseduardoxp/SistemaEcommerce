62f1bbe498c13387019902e586c309fe7d1dcff5
CarlosEduardo <CarlosEduardo@CarlosEdNote>
2016-03-07 21:47:14 -0300

    Inclusão de uma lista estática.

--- src/model/facade/rs/ClienteFacade.java
+++ src/model/facade/rs/ClienteFacade.java
@@ -1,11 +1,13 @@
 package model.facade.rs;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -18,20 +20,31 @@ import model.domain.Cliente;
 	   MediaType.APPLICATION_XML})
 public class ClienteFacade {
 	
-	@GET
-	public List<Cliente> getClientes() {
-		return Arrays.asList(new Cliente(1,"carlos","carlos@gmail.com"),
-							 new Cliente(2,"fulano","fulano@gmail.com"));
+	private static List<Cliente> clientes = new ArrayList<Cliente>();
+	
+	static {
+		clientes.add(new Cliente(1,"carlos","carlos@gmail.com"));
+		clientes.add(new Cliente(2,"fulano","fulano@gmail.com"));
+	}
+
+	@POST
+	public Cliente salvar(Cliente cliente) {
+		clientes.add(cliente);
+		return cliente;
 	}
 	
-	public List<Cliente> getClientes(Cliente cliente) {
-		if (cliente.getCodigo() == null) {
-			return Arrays.asList(new Cliente(0,"cliente não existe",""));
-		}
-		if (cliente.getCodigo() == 1) {
-			return Arrays.asList(new Cliente(1,"carlos","carlos@gmail.com"));
+	@GET
+	public List<Cliente> getClientes() {
+		return clientes;
+	}
+	
+	@GET
+	@Path("/{codigo}")
+	public Cliente getClientes(@PathParam("codigo") Integer codigo) {
+		if (clientes.get(codigo) != null) {
+			return clientes.get(codigo);
 		} else {
-			return Arrays.asList(new Cliente(3,"novo cliente","novo@gmail.com"));
+			return new Cliente(3,"novo cliente","novo@gmail.com");
 		}
 	}
 
